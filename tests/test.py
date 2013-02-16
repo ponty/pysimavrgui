65b20917d8ff1922745d593cc80ec88df3863368
ponty <ponty@home>
2013-02-16 19:19:03 +0100

    remove tests

--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,28 @@
 from nose.tools import eq_, timed
 from pysimavrgui.examples.sim import lcd, ledramp, sgm7
+import time
 
 VISIBLE = 0
 
-# fails!
-# def test_speed():
-#    led_ex.run_sim(speed=0.001, fps=20, timeout=0.001, visible=VISIBLE)
-#    led_ex.run_sim(speed=0.01, fps=1, timeout=0.01, visible=VISIBLE)
-#    led_ex.run_sim(speed=1, fps=1, timeout=0.1, visible=VISIBLE)
-#    led_ex.run_sim(speed=10, fps=100, timeout=1, visible=VISIBLE)
 
-
-@timed(10)
-def test_example_led():
-    ledramp.run_sim(speed=1, timeout=0.5, visible=VISIBLE)
-
+#def test_speed():
+#    ledramp.run_sim(speed=0.001, fps=20, timeout=0.001, visible=VISIBLE)
+#    ledramp.run_sim(speed=0.01, fps=1, timeout=0.01, visible=VISIBLE)
+#    ledramp.run_sim(speed=1, fps=1, timeout=0.1, visible=VISIBLE)
+#    ledramp.run_sim(speed=10, fps=100, timeout=1, visible=VISIBLE)
+#
+#
 #@timed(10)
-# def test_example_lcd():
-# TODO: lcd fails, no terminate()??
+#def test_example_led():
+#    ledramp.run_sim(speed=1, timeout=0.1, visible=VISIBLE)
+#
+#
+#@timed(10)
+#def test_example_lcd():
 #    lcd.run_sim(speed=0.1, timeout=0.3, visible=VISIBLE)
-
-# deadlock
+#
+#
 #@timed(10)
-# def test_example_sgm7():
+#def test_example_sgm7():
 #    sgm7.run_sim(speed=1, timeout=0.5, visible=VISIBLE)
 
-# fails!
-# because of lcd
-# def test_example_all():
-#    test_example_led()
-#    test_example_lcd()
-#    test_example_sgm7()
-#    test_example_led()
-#    test_example_lcd()
-#    test_example_sgm7()
